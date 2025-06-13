cf0b2e53580ce4b359630c4d5e94c28066186a22
gabri <gabri@DESKTOP-S6BLE95>
2025-06-13 15:48:39 -0300

    Finalizado javascript do projeto

---
+++ index.js
@@ -0,0 +1,78 @@
+const dadosCardControleMedicamentos = {
+    titulo: 'Controle de Medicamentos',
+    imagem: 'https://i.imgur.com/8u94HEA.png',
+    gif: 'https://i.imgur.com/SDgwx8m.gif',
+    urlRepositorio: 'https://github.com/academiadoprogramador-fullstack/controle-de-medicamentos-2025',
+    stack: [
+        'https://skillicons.dev/icons?i=html&theme=light',
+        'https://skillicons.dev/icons?i=css&theme=light',
+        'https://skillicons.dev/icons?i=cs&theme=light',
+        'https://skillicons.dev/icons?i=net&theme=light',
+    ],
+    descricao: ``
+};
+
+
+
+let cardSelecionado;
+
+function carregarDadosCardAtual() {
+    if (!cardSelecionado)
+        throw new Error('Card não selecionado.');
+
+    const imgCard = document.getElementById('imgCard');
+    const stackCard = document.getElementById('stackCard');
+    const tituloCard = document.getElementById('tituloCard');
+    const descricaoCard = document.getElementById('descricaoCard');
+    const urlRepositorio = document.getElementById('urlRepositorio');
+
+    imgCard.src = cardSelecionado.imagem;
+    tituloCard.textContent = cardSelecionado.titulo;
+    descricaoCard.textContent = cardSelecionado.descricao;
+    urlRepositorio.href = cardSelecionado.urlRepositorio;
+
+    stackCard.innerHTML = '';
+
+    for (let item of cardSelecionado.stack) {
+        const iconeStack = document.createElement('img');
+        iconeStack.width = 35;
+        iconeStack.src = item;
+
+        stackCard.appendChild(iconeStack);
+    }
+}
+
+function selecionarPrimeiroCard() {
+    if (cardSelecionado == dadosCardControleMedicamentos)
+        return;
+
+    cardSelecionado = dadosCardControleMedicamentos;
+    carregarDadosCardAtual();
+}
+
+function selecionarSegundoCard() {
+    if (cardSelecionado == dadosCardGestaoEquipamentos)
+        return;
+
+    cardSelecionado = dadosCardGestaoEquipamentos;
+    carregarDadosCardAtual();
+}
+
+function abrirModalProjeto() {
+    document.getElementById('portfolioModalLabel').textContent = cardSelecionado.titulo;
+    document.getElementById('portfolioModalImage').src = cardSelecionado.gif;
+}
+
+function main() {
+    const btnSelecionarPrimeiroCard = document.getElementById("btnSelecionarPrimeiroCard");
+    const btnSelecionarSegundoCard = document.getElementById("btnSelecionarSegundoCard");
+    const btnVerProjeto = document.getElementById("btnVerProjeto")
+
+    btnSelecionarPrimeiroCard.addEventListener('click', selecionarPrimeiroCard);
+    btnSelecionarSegundoCard.addEventListener('click', selecionarSegundoCard);
+    btnVerProjeto.addEventListener('click', abrirModalProjeto);
+
+    selecionarPrimeiroCard();
+};
+
+document.addEventListener('DOMContentLoaded', main);
